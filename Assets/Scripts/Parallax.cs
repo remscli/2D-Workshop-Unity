4b2ddf52195938252d42cb4ec006fe9ca07b6713
Rémy <remy@sleede.com>
2016-10-25 11:48:05 +0200

    add parallax effect

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	public float factor = 0.2f;
+	Transform cameraTransform;
+	Vector3 originalPos;
+	Vector3 originalCameraPos;
+
+	void Start () {
+		originalPos = transform.position;
+		cameraTransform = Camera.main.transform;
+		originalCameraPos = cameraTransform.position;
+	}
+
+	void LateUpdate () {
+		float newPosX = (cameraTransform.position.x - originalCameraPos.x) * (1 - factor);
+		transform.position = new Vector3( originalPos.x + newPosX, originalPos.y );
+	}
+}
