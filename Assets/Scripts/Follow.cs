eee9e9b4022793ec340560aee7e8ea8f212aa51b
Rémy <remy@sleede.com>
2016-10-25 11:59:40 +0200

    smooth hero following

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraCtrl : MonoBehaviour {
-
-	public GameObject Hero;
+public class Follow : MonoBehaviour {
 
 	Vector3 offset;
+	Vector3 velocity = Vector3.zero;
 	float maxCameraPosX;
+
+	public GameObject Target;
+	public float smoothTime = 0.3F;
 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - Hero.transform.position;
+		offset = transform.position - Target.transform.position;
 		GameObject terrain = GameObject.Find ("terrain-collider");
 		float sceneSize = terrain.GetComponent<PolygonCollider2D> ().bounds.size.x;
 
@@ -23,10 +25,10 @@ public class CameraCtrl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 tmpPosition = transform.position;
-		tmpPosition.x = Hero.transform.position.x + offset.x;
+		Vector3 newPosition = transform.position;
+		newPosition.x = Target.transform.position.x + offset.x;
 
-		if (tmpPosition.x < 0 || tmpPosition.x > maxCameraPosX) return;
-		transform.position = tmpPosition;
+		if (newPosition.x < 0 || newPosition.x > maxCameraPosX) return;
+		transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
 	}
 }
